b09a5f09e58cf5e3c701191f4428f61229f0a7b3
Sohel Zerdoumi <sohel.zerdoumi@gmail.com>
2016-07-23 22:51:46 -0100

    rename script

--- main.user.js
+++ main.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Uplea
+// @name         Website Downloader
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
